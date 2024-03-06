5bee221dc6a9ba50c22430f6001e320bd1f34744
kshileeva <kshileeva@gmail.com>
2024-03-06 19:37:29 +0100

    hovertools

--- read_csv.py
+++ read_csv.py
@@ -1,15 +1,52 @@
-import os
+from bokeh.models import ColumnDataSource, LinearAxis, Range1d
+from bokeh.plotting import figure, show
+from numpy import arange, linspace, pi, sin
 import pandas as pd
+import os
 directory = "assignment1_data"
-
-dfs = []
-
+dfs_rating = []
+dfs_crashes = []
 for filename in os.listdir(directory):
     if filename.startswith("stats_ratings_") and filename.endswith("_overview.csv"):
         filepath = os.path.join(directory, filename)
         df = pd.read_csv(filepath, encoding='UTF-16')
-        dfs.append(df)
+        dfs_rating.append(df)
+    if filename.startswith("stats_crashes_") and filename.endswith("_overview.csv"):
+        filepath = os.path.join(directory, filename)
+        df = pd.read_csv(filepath, encoding='UTF-16')
+        dfs_crashes.append(df)
+merged_rating = pd.concat(dfs_rating, ignore_index=True)
+merged_rating = merged_rating.fillna(0)
+merged_crash = pd.concat(dfs_crashes, ignore_index=True)
+ratings = merged_rating.drop(['Package Name'], axis=1)
+crashes = merged_crash.drop(['Package Name'], axis=1)
+merged_df = pd.merge(crashes, ratings, on='Date', how='inner')
+merged_df['Date'] = pd.to_datetime(merged_df['Date'])
+merged_df['Crashes'] = merged_df['Daily Crashes'] + merged_df['Daily ANRs']
+merged_df.drop(['Daily Crashes', 'Daily ANRs'], axis=1, inplace=True)
 
-print(df)
+x = arange(-2*pi, 2*pi, 0.2)
+y = merged_df['Crashes']
+y2 = merged_df['Daily Average Rating']
+dates = pd.date_range(start='2024-01-01', periods=len(x))
 
+source = ColumnDataSource(data={'x': merged_df['Date'], 'y': y, 'y2': y2})
 
+# Create figure
+p = figure(x_axis_type='datetime', y_axis_label='Crashes', width=800, height=500)
+
+# Plot Crashes on primary y-axis
+p.scatter('x', 'y2', source=source, color="navy", size=8)
+
+# Define the range for the secondary y-axis (ratings)
+p.extra_y_ranges = {"ratings": Range1d(start=0, end=5)}
+
+# Plot Ratings on secondary y-axis
+p.scatter('x', 'y', source=source, color="crimson", size=8, y_range_name="ratings")
+
+# Add secondary y-axis to the plot
+ax2 = LinearAxis(y_range_name="ratings", axis_label="Rating")
+p.add_layout(ax2, 'right')
+
+# Show the plot
+show(p)
--- scatter_plot.py
+++ scatter_plot.py
@@ -1,5 +1,5 @@
 from bokeh.plotting import figure, show
-from bokeh.models import LogAxis, DataRange1d, ColumnDataSource, HoverTool, FixedTicker
+from bokeh.models import LinearAxis, DataRange1d, ColumnDataSource, HoverTool, LogAxis
 import os
 import pandas as pd
 directory = "assignment1_data"
@@ -23,10 +23,12 @@ merged_df = pd.merge(crashes, ratings, on='Date', how='inner')
 merged_df['Date'] = pd.to_datetime(merged_df['Date'])
 merged_df['Crashes'] = merged_df['Daily Crashes'] + merged_df['Daily ANRs']
 merged_df.drop(['Daily Crashes', 'Daily ANRs'], axis=1, inplace=True)
+
 source = ColumnDataSource(merged_df)
 p = figure(x_axis_type='datetime', x_axis_label='Date', y_axis_label='Daily Average Rating', width=800, height=500)
 rating = p.scatter(x='Date', y='Daily Average Rating', source=source, color='blue')
-crashes = p.scatter(x='Date', y='Crashes', source=source, color='red', y_range_name="second_y_axis")
+crashes = p.scatter(x='Date', y='Crashes', source=source, color='red')
+second_y_range = DataRange1d(start=0, end=85)
 tooltips_rating = [
     ("Date", "@Date{%F}"),
     ("Daily Average Rating", "@{Daily Average Rating}{1.1}")
@@ -39,7 +41,6 @@ tooltips_crashes = [
 hover_rating = HoverTool(renderers=[rating], tooltips=tooltips_rating, formatters={'@Date': 'datetime'})
 hover_crashes = HoverTool(renderers=[crashes], tooltips=tooltips_crashes, formatters={'@Date': 'datetime'})
 p.add_tools(hover_rating, hover_crashes)
-p.yaxis.bounds = (0, 5)
-p.extra_y_ranges = {"second_y_axis": DataRange1d(start=0, end=85)}
+p.extra_y_ranges = {"second_y_axis": second_y_range}
 p.add_layout(LogAxis(y_range_name="second_y_axis", axis_label="Crashes"), 'right')
 show(p)
