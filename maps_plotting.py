ae0f8b70113432919aa01659fa23dc80e9a903f2
kshileeva <kshileeva@gmail.com>
2024-03-12 22:04:33 +0100

    gitignore datasets and 2 map plots no color

--- maps_plotting.py
+++ maps_plotting.py
@@ -1,8 +1,8 @@
 import os
 import geopandas as gpd
-from bokeh.layouts import row
+from bokeh.layouts import row, column
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import GeoJSONDataSource, HoverTool, Slider, CustomJS, Button, LinearColorMapper, ColorBar
+from bokeh.models import GeoJSONDataSource, HoverTool, Slider, CustomJS
 import pandas as pd
 import country_converter as coco
 
@@ -72,7 +72,8 @@ code_country = converting_countrycode(calculate_weekly_average())
 code_sale = converting_countrycode(sum_of_sales())
 
 
-def geodata_country(data: pd.DataFrame, shapefile='ne_110m_admin_0_countries_lakes/ne_110m_admin_0_countries_lakes.shp'):
+def geodata_country(data: pd.DataFrame,
+                    shapefile='ne_110m_admin_0_countries_lakes/ne_110m_admin_0_countries_lakes.shp'):
     gdf = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry']]
     gdf.columns = ['country', 'country_code', 'geometry']
     gdf = gdf.merge(data, on='country_code', how='left')
@@ -80,17 +81,15 @@ def geodata_country(data: pd.DataFrame, shapefile='ne_110m_admin_0_countries_lak
     return gdf
 
 
-def get_geodatasource(gdf):
+def get_datasource(gdf):
     import json
     json_data = json.dumps(json.loads(gdf.to_json()))
     return GeoJSONDataSource(geojson=json_data)
 
 
-# def update_data(data, attr, old, new):
-#     selected_date = slider.value
-#     new_data = data[data['Week'] == selected_date]
-#     new_source = get_geodatasource(new_data)
-#     source.geojson = new_source.geojson
+def filter_weekly_data(df, selected_week):
+    filtered_df = df.loc[df['Week'] == selected_week]
+    return filtered_df
 
 
 def plot_map():
@@ -98,25 +97,34 @@ def plot_map():
     p2 = figure(title="Weekly Sales per Country", height=300, width=750)
     country_geo = geodata_country(code_country)
     sale_geo = geodata_country(code_sale)
-    source_country = get_geodatasource(country_geo)
-    source_sales = get_geodatasource(sale_geo)
+    source_country = get_datasource(country_geo)
+    source_sales = get_datasource(sale_geo)
     p1.patches('xs', 'ys', source=source_country, line_color='black', line_width=0.5, fill_alpha=1)
     p2.patches('xs', 'ys', source=source_sales, line_color='black', line_width=0.5, fill_alpha=1)
     hover1 = HoverTool(
-        tooltips=[("Country", "@country"), ("Rating", "@Daily Average Rating")],
+        tooltips=[("Country", "@country"), ("Rating", "@{Daily Average Rating}{0,0.00}")],
         formatters={'Week': 'numeral'})
     p1.add_tools(hover1)
     hover2 = HoverTool(
-        tooltips=[("Country", "@country"), ("Amount, EUR", "@Amount (Merchant Currency)")],
+        tooltips=[("Country", "@country"), ("Amount, EUR", "@{Amount (Merchant Currency)}{0,0.00}")],
         formatters={'Week': 'numeral'})
     p2.add_tools(hover2)
-    slider = Slider(start=1, end=24, value=24, step=1, title="Week", width=110)
-    palette = ['#008000', '#FFC500', '#ff0000', '#c70000']
-    color_mapper = LinearColorMapper(palette=palette, low=0, high=4, nan_color='#838383')
-    from bokeh.layouts import column
+    slider = Slider(start=22, end=52, value=22, step=1, title="Week", width=110)
+
+    def update_source_data(attr, old, new):
+        selected_week = slider.value  # Assuming you have defined the slider widget
+        new_data_country = filter_weekly_data(code_country, selected_week)
+        new_data_sales = filter_weekly_data(code_sale, selected_week)
+        source_country.geojson = get_datasource(geodata_country(new_data_country)).geojson
+        source_sales.geojson = get_datasource(geodata_country(new_data_sales)).geojson
+
+    slider.on_change('value', update_source_data)
+
     layout = column(row(p1, p2), slider)
+
     output_file('world_map.html')
     show(layout)
 
 
 plot_map()
+
