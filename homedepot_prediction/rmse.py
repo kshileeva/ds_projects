e1ad3a12704e38066aa82e0fdbc38e1a4f0ea5db
kshileeva <kshileeva@gmail.com>
2024-04-10 22:31:31 +0200

    updated rmse due to another random state

--- homedepot_prediction/rmse.py
+++ homedepot_prediction/rmse.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn_rand_forest import X_train, y_train, clf
 
-X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X_train, y_train, test_size=0.2, random_state=0)
 clf.fit(X_train_split, y_train_split)
 
 y_pred_split = clf.predict(X_test_split)
