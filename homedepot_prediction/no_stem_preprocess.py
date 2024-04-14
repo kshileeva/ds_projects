890e751a6819e9ae4304bbd25c40c247004cef6e
kshileeva <kshileeva@gmail.com>
2024-04-14 20:42:39 +0200

    no stemming fixed

--- homedepot_prediction/no_stem_preprocess.py
+++ homedepot_prediction/no_stem_preprocess.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
-
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 df_train = pd.read_csv('depot_data/train.csv', encoding="ISO-8859-1")
 df_test = pd.read_csv('depot_data/test.csv', encoding="ISO-8859-1")
@@ -15,35 +16,38 @@ def str_common_word(str1, str2):
     return sum(int(str2.find(word) >= 0) for word in str1.split())
 
 
-df_all = pd.concat((df_train, df_test), axis=0, ignore_index=True)
+df_all_ns = pd.concat((df_train, df_test), axis=0, ignore_index=True)
 
-df_all = pd.merge(df_all, df_pro_desc, how='left', on='product_uid')
+df_all_ns = pd.merge(df_all_ns, df_pro_desc, how='left', on='product_uid')
 
-df_all['search_term'] = df_all['search_term'].map(lambda x: x.lower())
-df_all['product_title'] = df_all['product_title'].map(lambda x: x.lower())
-df_all['product_description'] = df_all['product_description'].map(lambda x: x.lower())
+df_all_ns['search_term'] = df_all_ns['search_term'].map(lambda x: x.lower())
+df_all_ns['product_title'] = df_all_ns['product_title'].map(lambda x: x.lower())
+df_all_ns['product_description'] = df_all_ns['product_description'].map(lambda x: x.lower())
 
-df_all['len_of_query'] = df_all['search_term'].map(lambda x: len(x.split())).astype(np.int64)
+df_all_ns['len_of_query'] = df_all_ns['search_term'].map(lambda x: len(x.split())).astype(np.int64)
 
-df_all['product_info'] = df_all['search_term']+"\t"+df_all['product_title']+"\t"+df_all['product_description']
+df_all_ns['product_info'] = df_all_ns['search_term'] + "\t" + df_all_ns['product_title'] + "\t" + df_all_ns['product_description']
 
-df_all['word_in_title'] = df_all['product_info'].map(lambda x: str_common_word(x.split('\t')[0], x.split('\t')[1]))
-df_all['word_in_description'] = df_all['product_info'].map(lambda x: str_common_word(x.split('\t')[0],
-                                                                                     x.split('\t')[2]))
+df_all_ns['word_in_title'] = df_all_ns['product_info'].map(lambda x: str_common_word(x.split('\t')[0], x.split('\t')[1]))
+df_all_ns['word_in_description'] = df_all_ns['product_info'].map(lambda x: str_common_word(x.split('\t')[0],
+                                                                                           x.split('\t')[2]))
 
-df_all = df_all.drop(['search_term', 'product_title', 'product_description', 'product_info'], axis=1)
+df_all_ns = df_all_ns.drop(['search_term', 'product_title', 'product_description', 'product_info'], axis=1)
 
-df_train = df_all.iloc[:num_train]
-df_test = df_all.iloc[num_train:]
+df_train = df_all_ns.iloc[:num_train]
+df_test = df_all_ns.iloc[num_train:]
 id_test = df_test['id']
 
-y_train = df_train['relevance'].values
-X_train = df_train.drop(['id', 'relevance'], axis=1).values
-X_test = df_test.drop(['id', 'relevance'], axis=1).values
+y_train_no_stem = df_train['relevance'].values
+X_train_no_stem = df_train.drop(['id', 'relevance'], axis=1).values
+X_test_no_stem = df_test.drop(['id', 'relevance'], axis=1).values
 
-rf = RandomForestRegressor(n_estimators=15, max_depth=6, random_state=0)
-clf = BaggingRegressor(rf, n_estimators=45, max_samples=0.1, random_state=25)
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
+rf_no_stem = RandomForestRegressor(n_estimators=15, max_depth=6, random_state=0)
+clf_no_stem = BaggingRegressor(rf_no_stem, n_estimators=45, max_samples=0.1, random_state=25)
+X_train_split_ns, X_test_split_ns, y_train_split_ns, y_test_split_ns = train_test_split(X_train_no_stem, y_train_no_stem, test_size=0.2, random_state=0)
+clf_no_stem.fit(X_train_split_ns, y_train_split_ns)
 
-pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv('submission.csv', index=False)
+y_pred_split_ns = clf_no_stem.predict(X_test_split_ns)
+rmse_no_stem = np.sqrt(mean_squared_error(y_test_split_ns, y_pred_split_ns))
+# pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv('submission.csv', index=False)
+print(rmse_no_stem)
--- homedepot_prediction/rmse.py
+++ homedepot_prediction/rmse.py
@@ -2,9 +2,9 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 from sklearn.model_selection import train_test_split
 #  from sklearn_rand_forest
-from no_stem_preprocess import X_train, y_train, clf
+from no_stem_preprocess import X_train_no_stem, y_train_no_stem, clf
 
-X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X_train, y_train, test_size=0.2, random_state=0)
+X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X_train_no_stem, y_train_no_stem, test_size=0.2, random_state=0)
 clf.fit(X_train_split, y_train_split)
 
 y_pred_split = clf.predict(X_test_split)
